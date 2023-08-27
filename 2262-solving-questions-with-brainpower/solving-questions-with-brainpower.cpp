167f98530da3ba036be6c94be5a2eab5e256b93c
Abhishek Gupta <121763140+Tesla-g@users.noreply.github.com>
2023-08-27 21:34:15 +0530

    Time: 292 ms (65.44%) | Memory: 121.5 MB (50.74%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,19 +1,23 @@
+
 class Solution {
 public:
+
     int n;
-    vector<long long> t; // Declare a memoization array
-
-    long long s(int i, vector<vector<int>>& nums) {
-        if (i >= n) return 0;
-        if (t[i] != -1) return t[i];
-        long long taki = nums[i][0] + s(nums[i][1] + i + 1, nums);
-        long long skip = s(i + 1, nums);
-        return t[i] = max(taki, skip);
+    
+    long long s(int i,vector<vector<int>>& nums , vector<long long int>&t){
+        
+        if(i>=n) return 0;
+        if(t[i]!=-1)return t[i];
+        long long  int taki=nums[i][0]+s(nums[i][1]+i+1,nums,t);
+       long long  int skip=s( i+1,nums,t);
+        return  t[i]= max(taki,skip);
+        
+        
     }
-
     long long mostPoints(vector<vector<int>>& questions) {
-        n = questions.size();
-        t.resize(n, -1); // Initialize the memoization array
-        return s(0, questions);
+        n=questions.size();
+           vector<long long int> t(n+1,-1);
+        // memset(t,-1,sizeof(t));
+        return s( 0,questions,t);
     }
 };
