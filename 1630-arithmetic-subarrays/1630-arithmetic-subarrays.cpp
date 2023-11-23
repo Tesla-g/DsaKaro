8e34aeee604987a887ca1fc0a10ae0bb9ed12081
Abhishek Gupta <121763140+cppcoderr@users.noreply.github.com>
2023-11-23 11:35:25 +0530

    Time: 63 ms (51.95%), Space: 39 MB (24.84%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -2,35 +2,49 @@ class Solution
 {
     public:
 
-   bool check(vector<int> &nums) {
-    int n = nums.size();
+        bool check(vector<int> &nums)
+        {
+            int n = nums.size();
 
-    for (int i = 0; i < n - 2; i++) {
-        if (!(abs(nums[i] - nums[i + 1]) == abs(nums[i + 1] - nums[i + 2]))) {
-            return false;
+            for (int i = 0; i < n - 2; i++)
+            {
+                if ((abs(nums[i] - nums[i + 1]) != abs(nums[i + 1] - nums[i + 2])))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
-    }
-
-    return true;
-}
-
-vector<bool> f(vector<int> &nums, vector<int> &l, vector<int> &r) {
-    vector<bool> ans;
-    int m = l.size();
-
-    for (int j = 0; j < m; j++) {
-        vector<int> v(nums.begin() + l[j], nums.begin() + r[j] + 1);
-        sort(v.begin(),v.end());
-        bool isValid = check(v); // Check validity for each subarray
-
-        ans.push_back(isValid);
-    }
-
-    return ans;
-}
-
-    vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l, vector< int > &r) {
+    vector<bool> f(vector<int> &nums, vector<int> &l, vector< int > &r)
+    {
       
-      return f(nums,l,r);
+        int m = l.size();
+        vector<bool> ans(m,0);
+        for (int j = 0; j < m; j++)
+          {   
+        vector<int> v;
+            for (int i = l[j]; i <= r[j]; i++)
+            {
+                v.push_back(nums[i]);
+            }
+          
+         ///////////////after pushing all those value into the vector 
+          sort(v.begin(), v.end());
+           	///////////////tru to checj for that particualr combination
+            if (check(v))
+            {
+              ans[j]=true;
+              
+            }
+        }
+
+        return ans;
+    }
+
+    vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l, vector< int > &r)
+    {
+
+        return f(nums, l, r);
     }
 };
