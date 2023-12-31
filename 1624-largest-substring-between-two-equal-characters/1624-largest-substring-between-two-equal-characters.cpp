b24ed2956adf9ee5ea44764b48edad079463cce1
Abhishek Gupta <121763140+cppcoderr@users.noreply.github.com>
2023-12-31 13:13:27 +0530

    Time: 0 ms (100.00%), Space: 7 MB (11.97%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-int f(string &s){
+int solve(string &s){
+
   int n=s.size();
+  // store the last occurence of the occurence of the characete 
+
   int result=-1;
+
+  // change the last occurence of the character
+
+  unordered_map<char,int>mp;
   for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-      if(s[i]==s[j]){
-        result=max(result,j-i-1);
-      }
+    char ch=s[i];
+    if(mp.find(ch)==mp.end()){
+      mp[ch]=i;
     }
+    else{
+      // current  index-pervious occurring  index-1
+      // you can not fall into the  love until you are not earning the decent amoutn of  money  
+      // money is the only trth and noting is 
+      result=max(result , i-mp[ch]-1);
+    }
+    // store the last occuence of the chareacter 
+
   }
   return result;
-
 }
 
 
     int maxLengthBetweenEqualCharacters(string s) {
-        return f(s);
+        return solve(s);
     }
 };      
