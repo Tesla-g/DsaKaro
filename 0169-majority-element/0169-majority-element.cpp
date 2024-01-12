092f94bf4dc66335de3cb2446c1c65e8d527000b
Abhishek Gupta <121763140+cppcoderr@users.noreply.github.com>
2024-01-12 23:19:28 +0530

    Time: 23 ms (12.00%), Space: 20.2 MB (13.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,44 @@
 
 class Solution {
 public:
-  int majority(vector<int>&v){
-  int n=v.size();
-  unordered_map<long long int,long long int >mp;
-  for(auto & e: v){
-    mp[e]++;
+    int countmostfreq(vector<int>&v) {
+  sort(v.begin(),v.end());
+  int n = v.size();
+  if (n == 0) {
+    return -1;
   }
-   long long int freq=floor(n/2);
-  for(auto & ele:mp){
-    if(ele.second>freq){
-      return ele.first;
+
+  if (n == 1) {
+    return v[0];
+  }
+
+  int cnt = 1;
+  int candidate = v[0];
+  int prevCnt = 0;
+  int mostfreq = candidate;
+
+  for (int i = 1; i < n; i++) {
+    if (v[i] == candidate) {
+      cnt++;
+    } else {
+      if (prevCnt < cnt) {
+        prevCnt = cnt;
+        mostfreq = candidate;
+      }
+      cnt = 1;
+      candidate = v[i];
     }
   }
-    return -1;
+
+  // Handle the last element/group
+  if (prevCnt < cnt) {
+    mostfreq = candidate;
+  }
+  return mostfreq;
 }
 
 
     int majorityElement(vector<int>& nums) {
-        return majority(nums);
+      return countmostfreq(nums);
     }
 };
