d265c4e4323f0f6a47013c4c34a4bcae7d450ef9
Abhishek Gupta <121763140+cppcoderr@users.noreply.github.com>
2024-01-16 00:38:28 +0530

    Time: 624 ms (29.39%), Space: 178.5 MB (44.98%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+  vector<vector<int>> solution(vector<vector<int>> & m){
+  map<int,int>win;
+  map<int,int>lost;
+  for(auto &e:m ){
+    win[e[0]]++;
+    lost[e[1]]++;
+  }
+  
+  vector<vector<int>>ans(2);
+  for(auto& e: win){
+    if(lost[e.first]==0){
+      ans[0].push_back(e.first);
+    }
+  }
+  for(auto &ele: lost){
+    if(ele.second==1){
+      ans[1].push_back(ele.first);
+    }
+  }
+
+  return ans;
+}
+
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int,int>lost_map;
-        for(int i=0;i<matches.size();i++)
-        {
-            int lose=matches[i][1];
-        lost_map[lose]++;
-        }
-        vector<int>notLost;
-        vector<int>onceLost;
-        for(int i=0;i<matches.size();i++)
-        {
-           int winner=matches[i][0];
-           int looser=matches[i][1];
-           if(lost_map.find(winner)==lost_map.end()) 
-           {
-               notLost.push_back(winner);
-               lost_map[winner]=2;
-
-           }
-           if(lost_map[looser]==1)
-           onceLost.push_back(looser);
-
-        }
-        sort(notLost.begin(),notLost.end());    
-        sort(onceLost.begin(),onceLost.end());
-
-        return {notLost,onceLost};
-
-
-
-        
+        return solution(matches);
     }
 };
