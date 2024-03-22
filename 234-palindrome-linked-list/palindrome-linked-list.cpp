f95bd86579c1e184855bb598ece722aa8a676f49
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-03-22 15:00:05 +0530

    Time: 176 ms (48.94%) | Memory: 130.9 MB (12.57%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,32 +10,27 @@
  */
 class Solution {
 public:
+    bool isPalindrome(vector<int>&v){
+        int n=v.size();
+//         check if th e
+       int i=0, j=n-1;
+        while(i<=j){
+            if(v[i]!=v[j]){
+                return false;
+            }
+              i++;
+              j--;
+        }
+        return true;
+    }
     bool isPalindrome(ListNode* head) {
-       vector<int> v;
-        
+        if(!head) return false;
+        vector<int>v;
         ListNode * t=head;
-        while(t!=NULL){
-        v.push_back(t->val);
-        t=t->next;
-        }
-        vector<int> v2;
-        int n =v.size();
-        int i=0;
-        int j=v.size()-1;
-        while(j>=0){
-            v2.push_back(v[j]);
-           j--;
-        }
-        
-        
-        for(int i =0;i<n;i++){
-            
-            if(v[i]!=v2[i]) return false;
-        }
-        
-        return true; 
-       
-            
-        
+       while(t!=nullptr){
+            v.push_back(t->val);
+           t=t->next;
+       }
+        return isPalindrome(v);
     }
 };
