f59dcc7eb5dae3aa6bf5ce4dd82f92d4e0a2f728
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-03-22 15:30:30 +0530

    Time: 4 ms (84.34%) | Memory: 10.6 MB (5.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if(x<0)return false;
         string s=to_string(x);
         string t=s;
         reverse(t.begin(),t.end());
-        return t==s? 1: 0;
+        return s==t;
     }
 };
