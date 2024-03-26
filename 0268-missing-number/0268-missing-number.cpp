c8e3b42a0e83ad857f4c5e1cc031270f708c55de
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-03-27 00:44:03 +0530

    Time: 36 ms (5.15%), Space: 27.6 MB (6.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    bool search(vector<int>&nums,int &target){
-        int n=nums.size();
-        for(auto ele : nums){
-            if ( ele == target){
-                return 1;
-            }
-        }
-        return 0;
-    }
     int missingNumber(vector<int>& nums) {
-        for(int i=0; i<=nums.size();i++){
-            if(!search(nums,i)){
-                return i;
-            }
+        int n=nums.size();
+        unordered_set<int>st;
+        for(auto e: nums){
+            st.insert(e);
+        }
+        for(int i=0;i<=n;i++){
+           if(st.find(i)==st.end()){
+               return i;
+           }
         }
         return -1;
     }
