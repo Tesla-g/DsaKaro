83856ada9a14174037ed6752c09041ae9a187f42
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-05-20 21:53:56 +0530

    Time: 4 ms (55.14%) | Memory: 8.4 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-  
-    void findSubsets(int idx,vector<int>curr,vector<int>&nums,vector<vector<int>>&allSubsets){
-        if(idx==nums.size()){
-            allSubsets.push_back(curr);
-            return;
-        }
-        curr.push_back(nums[idx]);
-        findSubsets(idx+1,curr,nums,allSubsets);
-        curr.pop_back();
-        findSubsets(idx+1,curr,nums,allSubsets);
+    int solve (int i,vector<int>&nums,int Xor=0 ){
+        if(i==nums.size()) return Xor;
         
+        
+        
+        int include=solve(i+1,nums,Xor^nums[i]);
+        
+        
+        int exclude=solve(i+1,nums,Xor);
+        
+        return include+exclude;
     }
     int subsetXORSum(vector<int>& nums) {
-        vector<vector<int>>allSubsets;
-        vector<int>curr;
-        findSubsets(0,curr,nums,allSubsets);
-        int sum=0;
-        for(auto &sub: allSubsets){
-            int value=0;
-            for(auto &ele: sub){
-                value^=ele;
-            }
-            sum+=value;
-        }
-        return sum;
+        return solve(0,nums);
     }
 };
