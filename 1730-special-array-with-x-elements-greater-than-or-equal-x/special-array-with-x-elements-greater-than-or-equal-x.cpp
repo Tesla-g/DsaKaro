868a5d91c0304568c9b4d8b37b6f3e3eba1aef56
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-05-27 20:18:06 +0530

    Time: 5 ms (33.11%) | Memory: 10.2 MB (53.24%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-
-  
     int specialArray(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<=nums.size();i++){
-            auto idx=lower_bound(nums.begin(),nums.end(),i)-nums.begin();
-            if(n-idx==i) return i;
+       // do the binary search for the 
+        int l=0;
+        int r=nums.size();
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            auto idx =lower_bound(nums.begin(),nums.end(),mid)-nums.begin();
+            int cnt=n-idx;
+            if(cnt>mid){
+                l=mid+1;
+            }
+            else if(cnt<mid){
+                r=mid-1;
+            }
+            else{
+                return mid;
+            }
         }
-           return -1;
+        return -1;
     }
  
 };
