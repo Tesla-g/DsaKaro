1fa6bf608abc0f7f3784359fa4493221d4a80c2a
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-05-27 19:54:25 +0530

    Time: 2 ms (51.71%) | Memory: 10.1 MB (53.24%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
 
-    bool findElementGreaterThanX(int x,vector<int>&nums){
-//         how many elements which is greater than euqla to x
-        
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>=x){
-                cnt++;
-            }
-        }
-       return cnt==x;
-        
-    }
+  
     int specialArray(vector<int>& nums) {
-       auto  max=*max_element(nums.begin(),nums.end());
-        for(int i=0;i<=max;i++){
-            if(findElementGreaterThanX(i,nums)) return i;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=nums.size();i++){
+            auto idx=lower_bound(nums.begin(),nums.end(),i)-nums.begin();
+            if(n-idx==i) return i;
         }
            return -1;
     }
