ebe9bab7d4abe92c9dc003e30b0e5c3186884add
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-05-31 23:40:14 +0530

    Time: 8 ms (47.11%) | Memory: 12.6 MB (38.03%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto &ele : nums){
-            mp[ele]++;
-        }
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(nums.size()==2) return nums;
         vector<int>ans;
-        for(auto &v: mp){
-            if(v.second ==1 ){
-                ans.push_back(v.first);
+        if(nums[0]!=nums[1]) ans.push_back(nums[0]);
+        for(int i=1;i<=n-2;i++){
+                if(nums[i]!=nums[i-1] and nums[i]!=nums[i+1]){
+                    ans.push_back(nums[i]);
+                }
             }
+            if(nums[n-1]!=nums[n-2]) ans.push_back(nums[n-1]);
+                return ans;
         }
-        return ans;
-    }
+
 };
