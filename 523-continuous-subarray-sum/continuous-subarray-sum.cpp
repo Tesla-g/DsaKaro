f14622deeb6dedfecd6b72980fe906f727a65061
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-06-08 19:23:38 +0530

    Time: 178 ms (61.59%) | Memory: 134.5 MB (86.97%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -3,23 +3,16 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         if(n<2) return false;
-        vector<int>psum(n,0);
-        psum[0]=nums[0];
-        
-        for(int i=1;i<n;i++){
-            psum[i]=(psum[i-1]+nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            psum[i]=psum[i]%k;
-        }
+        int rSum=0; 
         unordered_map<int,int>mp;
-        mp.insert({0,0});
-//         to store the first occurence 
+        mp.insert({0,-1});
         for(int i=0;i<n;i++){
-            if(mp.find(psum[i])==mp.end()){
-                mp.insert({psum[i],i+1});
+            rSum+=nums[i];
+            int mod=rSum%k;
+            if(mp.find(mod)==mp.end()){
+                mp.insert({mod,i});
             }else{
-                if(i+1-mp[psum[i]]>=2){
+                if(i-mp[mod]>1){
                     return true;
                 }
             }
