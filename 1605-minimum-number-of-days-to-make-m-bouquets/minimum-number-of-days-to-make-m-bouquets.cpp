31fc800864b937e1d23addae7c18dc7d4584e237
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-06-19 21:48:50 +0530

    Time: 111 ms (63.43%) | Memory: 69 MB (28.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -17,11 +17,11 @@ public:
         return bloomcount;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int l=1;
-        int r=*max_element(begin(bloomDay),end(bloomDay));
+        int l= *min_element(begin(bloomDay),end(bloomDay));
+        int r= *max_element(begin(bloomDay),end(bloomDay));
         int result=-1;
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l+((r-l))/2;
             if(checkBookie(mid,m,k,bloomDay)>=m){
                 result=mid;
                 r=mid-1;
