8c6a5cd5186536d026e7677e06f49d0b98e2eaa2
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-08-14 20:23:04 +0530

    Time: 1901 ms (5.04%), Space: 497.3 MB (5.05%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
-    
-    // Find count of pairs having distance <= D
-    int slidingWindowCount(vector<int>& nums, int D) {
-        int count = 0;
-        int n = nums.size();
-        int i = 0;
-        int j = 1;
-
-        while (j < n) {
-            while (nums[j] - nums[i] > D) {
-                ++i;
-            }
-            count += j - i;
-            j++;
-        }
-        return count;
-    }
-
     int smallestDistancePair(vector<int>& nums, int k) {
-        sort(begin(nums), end(nums));
-        int n = nums.size();
-        int l = 0;
-        int r = nums[n - 1] - nums[0];
-        int result = 0;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            int count = slidingWindowCount(nums, mid);
-
-            if (count < k) {
-                l = mid + 1;
-            } else {
-                result = mid;  // Store the mid as a potential result
-                r = mid - 1;
+        int n=nums.size();
+        vector<int>vec;
+        for(size_t i=0; i<n; ++i){
+            for(size_t j=i+1; j<n; ++j){
+                int dist=abs(nums[i]-nums[j]);
+                vec.push_back(dist);
             }
-        }
-        return result;
+           
+        
+    }
+         nth_element(begin(vec), begin(vec)+k-1, end(vec));
+            return vec[k-1];
     }
 };
