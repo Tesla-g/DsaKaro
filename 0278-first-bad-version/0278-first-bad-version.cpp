641e8e1d8033c50d4faf927706d5eb58ece15c60
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-08-24 19:17:07 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (10.90%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,11 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-      for(int i=1; i<=n; i++){
-          if(isBadVersion(i)){
-              return i; 
-          }
-      }
-    return -1;
+        int low=1; int high=n; 
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isBadVersion(mid)){
+                // current is a bad verison fin dthe loweest mone
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 };
