9537405e26c32b1e595408a4d4061a45fdb89151
Abhishek Gupta <121763140+abhiiguptaa@users.noreply.github.com>
2024-08-28 21:44:06 +0530

    Time: 525 ms (18.08%), Space: 301.7 MB (5.14%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,30 +1,57 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int x, int y, int &res) {
-        int m = grid1.size(), n = grid1[0].size();
-        if(x < 0 || y < 0 || x >= m || y >= n) return;
-        if(grid1[x][y] == 0 && grid2[x][y] == 1) {
-            res = false;
+    typedef pair<int,int> P;
+    bool bfs(int i , int j , vector<vector<int>>&grid1, vector<vector<int>>&grid2){
+        vector<vector<int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
+        queue<P>que;
+        que.push({i,j});
+        // mark them as visted
+        grid2[i][j]=-1;
+        
+        bool result=true;
+        while(!que.empty()){
+            auto top=que.front();
+            que.pop();
+            // expolore all it's child
+            if(grid1[top.first][top.second]!=1){
+                result=false;
+            }
+            
+            // if the grid is not does not conat the one 
+            // but explore all of it's child
+            
+            
+            for(auto &ele: dir){
+                int newx=top.first+ele[0];
+                int newy=top.second+ele[1];
+                // explore all of it's natural 
+                // mark this as visite
+                if(newx>=0 and newy>=0 and newy<grid2[0].size() and newx<grid2.size() and grid2[newx][newy]==1){
+                    grid2[newx][newy]=-1;
+                    que.push({newx,newy}); 
+                }
+                
+            }
+
         }
-        if(grid2[x][y] == 0) return;
-        grid2[x][y] = 0;
-        dfs(grid1, grid2, x + 1, y, res);
-        dfs(grid1, grid2, x, y + 1, res);
-        dfs(grid1, grid2, x - 1, y, res);
-        dfs(grid1, grid2, x, y - 1, res);
+        return result;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int m = grid1.size(), n = grid1[0].size();
-        int ans = 0;
-        for(int i = 0; i < m ; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1) {
-                    int res = true;
-                    dfs(grid1, grid2, i, j, res);
-                    ans += res;
+        int count=0;
+        int m = grid2.size(); int n = grid2[0].size();
+        for(int i = 0; i<m ; ++i){
+            
+            for(int j= 0; j< n; ++j){
+                if(grid2[i][j]==1 and bfs(i,j,grid1, grid2)){
+                    count++;
                 }
             }
+            
         }
-        return ans;
+        
+        
+        return count; 
+
+ 
     }
 };
